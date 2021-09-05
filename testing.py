64f26d6dc85d1752b048b865da58b5dd0bc7b32b
sakharamg <213050027@iitb.ac.in>
2021-09-05 09:10:35 +0530

    testing.py has code to call q2 functions

--- testing.py
+++ testing.py
@@ -1,13 +1,9 @@
-from q1 import *
+##TEST Q1--DONE
+# from q1 import *
+# print(nList([1,2,3,4,5,7,6],1))
+# fibo=fib()
+# print(next(fibo))
 
-print(nList([1,2,3,4,5,7,6],3))
-
-
-fibo=fib()
-print(next(fibo))
-print(next(fibo))
-print(next(fibo))
-print(next(fibo))
-print(next(fibo))
-print(next(fibo))
-print(next(fibo))
+##TEST Q2--DONE
+# from q2 import *
+# print(prime(13))
