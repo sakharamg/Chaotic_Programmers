956a2c4ee37911974e72cd7d53d46555fd9803f5
sakharamg <213050027@iitb.ac.in>
2021-09-05 08:55:06 +0530

    Adding a file that imports the q1,q2,... files and calls functions :D

---
+++ testing.py
@@ -0,0 +1,13 @@
+from q1 import *
+
+print(nList([1,2,3,4,5,7,6],3))
+
+
+fibo=fib()
+print(next(fibo))
+print(next(fibo))
+print(next(fibo))
+print(next(fibo))
+print(next(fibo))
+print(next(fibo))
+print(next(fibo))
