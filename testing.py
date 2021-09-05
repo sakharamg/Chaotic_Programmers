eb1741df7878921af2b9fd34e87426054a0401b7
sakharamg <213050027@iitb.ac.in>
2021-09-05 10:14:18 +0530

    q5 worked correctly, added testing code in testing.py

--- testing.py
+++ testing.py
@@ -21,8 +21,32 @@
 #mapping=readWorkSheet("q4_sample_input.csv")
 #mapping=readWorkSheet("../chaotic_programmers_code/q4/q4_sample_input.csv")
 #print(calculateOfficeHrs(mapping))
+#print(type(calculateOfficeHrs(mapping)))##Check the data type if tuple or not
 #writeOfficeWorkSheet(mapping,"q4_out.csv")
 #writeOfficeWorkSheet(mapping,"../q4_out.csv")
 #writeOfficeWorkSheet(mapping,"../verification/q4_output/q4_out.csv")
 
-
+##TEST Q5--DONE
+#from q5 import *
+#name1 = "Ash Ketchum" 
+#accounts_info1 = {
+#	"p1" : 8787,
+#	"p2":987,
+#	"p3": 45678,
+#	"p4": 90000
+#}
+#name2 = "Misty "
+#accounts_info2 = {
+#"bank1" : 100068,
+#"bank2":6000798,
+#}
+#corrupt1 = BlackMoneyHolder(name1, accounts_info1);
+#corrupt2 = BlackMoneyHolder(name2, accounts_info2);
+#print(corrupt1)
+#print(corrupt2)
+#print(corrupt1<corrupt2,corrupt1==corrupt2,corrupt2>corrupt1,len(corrupt1),len(corrupt2))
+#print("Corrupt person1 total black: o^o",corrupt1.total_black_money())
+#print("Corrupt person2 total black: >.<",corrupt2.total_black_money())
+#corrupt1.update_amount("p1",9999)
+#print("Checking for amount update",corrupt1)
+#print("Person1 updated total black: \o-o/",corrupt1.total_black_money())
