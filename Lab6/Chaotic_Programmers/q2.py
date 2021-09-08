ce5c637c48d068258841ae9aeeaff59cd378f9da
sakharamg <213050027@iitb.ac.in>
2021-09-08 14:26:34 +0530

    Added Lab6 files

---
+++ Lab6/Chaotic_Programmers/q2.py
@@ -0,0 +1,15 @@
+class TWODMAT:
+    def nor_mat(arr):
+        #arr- a matrix of a given dimension MxN
+        
+        
+    def sum_fil(arr,k):
+        #arr-array of shape (n) 
+        #k-kernal size
+        
+        
+    def top_pos(arr,k):
+        #arr-a matrix of shape (m,n). 
+        #k-top k values
+
+if __name__ == '__main__':
