c14dee96863151d021738eea31fc0b4a7a201b27
sakharamg <213050027@iitb.ac.in>
2021-09-08 14:29:34 +0530

    Added folders for each of us

---
+++ Lab6/Abisek Temp work/q6.py
@@ -0,0 +1,22 @@
+import json
+
+
+def processData( input_path, output_path ):
+    '''  write your code here '''
+    print(input_path)
+    print(output_path)
+    
+
+
+if __name__ == "__main__":
+    '''
+        Remove this comment.
+        This is just for you to run and check your code.
+
+        To test your function we will import this file and call your function.
+        Before running give appropriate file path for path_to_input_json below. 
+    '''
+    path_to_input_json = './testcases/q6/q6_input.json'
+    path_to_output_json = './testcases/q6/q6_output.json'
+    processData( path_to_input_json, path_to_output_json )
+    
