1013ce3547c60ad53b399f3bbf82c42e6ee82ba6
sakharamg <213050027@iitb.ac.in>
2021-09-15 16:43:25 +0530

    Finished Q2

--- Lab6/Sakharam Temp Work/q1.py
+++ Lab6/Sakharam Temp Work/q1.py
@@ -5,17 +5,17 @@ import numpy as np
 # 'my_list': a list of 25 integers
 def avada_kedavra(my_list):
 	# Create an array 'a0' from this list 'my_list'
-
+	a0=np.array(my_list)
 	# print a0
 	print("a0 at beginning:\n{}".format(a0))
 
 	# reshape a0 to create a 5X5 matrix a1
-
+	a1=np.reshape(a0,(5,5))
 	# print a1
 	print("a1 at the beginning:\n{}".format(a1))
 
 	# now, change the central element of a1 to 0
-
+	a1[2,2]=0
 	# print a1
 	print("a1 after change:\n{}".format(a1))
 
@@ -24,12 +24,12 @@ def avada_kedavra(my_list):
 	print("reshaping doesn't create a copy of array. It just returns another view. So, changing one changed the other")
 
 	# make a copy of a1 and flatten it (call it a1cpy)
-
+	a1cpy=np.flatten(a1cpy)
 	# multiply each element of a1cpy by 0.7:
-
+	a1cpy*=0.7
 	# print a1cpy:
 	print("a1cpy\n{}".format(a1cpy))
-
+	
 	# print a1:
 	print("a1 after changing its copy:\n{}".format(a1))
 
--- Lab6/Sakharam Temp Work/q2.py
+++ Lab6/Sakharam Temp Work/q2.py
@@ -1,15 +1,17 @@
 class TWODMAT:
     def nor_mat(arr):
         #arr- a matrix of a given dimension MxN
-        
+        import numpy as np
+        return np.round((np.array(arr)-np.mean(np.array(arr),axis=0))/np.std(np.array(arr),axis=0),2)
         
     def sum_fil(arr,k):
         #arr-array of shape (n) 
         #k-kernal size
-        
+        import numpy as np
+        return np.cumsum(np.pad(arr, (0, k-1), 'constant', constant_values=(0,0)),axis=0)-np.cumsum(np.pad(arr, (k, 0), 'constant', constant_values=(0,0)))[0:len(arr)+k-1]
         
     def top_pos(arr,k):
         #arr-a matrix of shape (m,n). 
         #k-top k values
-
+	return np.flip(np.argsort(arr))[:,:k]
 if __name__ == '__main__':
