42520f88eee90e40e46a0666b67714720d5bf192
sakharamg <213050027@iitb.ac.in>
2021-09-27 10:42:48 +0530

    Added all files in Chaotic Programmers

--- Lab7/Chaotic_Programmers/q1_client.py
+++ Lab7/Chaotic_Programmers/q1_client.py
@@ -2,3 +2,8 @@
 write your code here.
 import what ever neccesary from xmlrpc module and nothing else.
 '''
+from xmlrpc.client import ServerProxy
+a=input("")
+b=input("")
+with ServerProxy("http://localhost:8080/") as proxy:
+	print(proxy.find_prime(int(a),int(b)))
